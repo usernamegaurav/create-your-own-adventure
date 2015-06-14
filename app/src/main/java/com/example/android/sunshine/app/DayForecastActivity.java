e49f103976aa879c9af37dfff03c84aa54d4408f
Gaurav Kumar <usernamegaurav@gmail.com>
2015-06-14 14:36:15 +0530

    add settings

--- app/src/main/java/com/example/android/sunshine/app/DayForecastActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/DayForecastActivity.java
@@ -41,6 +41,8 @@ public class DayForecastActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
             return true;
         }
 
